2f4f043552e960a1a837afaee00a4378435fe0af
rohan <halo.zero00@gmail.com>
2014-04-23 15:35:58 -0700

    Fixing server call, moving js to local file

--- server.rb
+++ server.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'httparty'
 require 'pry'
+require 'json'
 
 get '/locations' do
   safe_param_keys = [:n, :s, :e, :w]
@@ -8,6 +9,7 @@ get '/locations' do
   safe_param_keys.each do |key|
     safe_params[key] = params[key].to_f
   end
+
   url = "http://api.geonames.org/citiesJSON?"\
   "north=#{safe_params[:n]}&south=#{safe_params[:s]}&"\
   "east=#{safe_params[:e]}&west=#{safe_params[:w]}&lang=de&username=halozero"
@@ -19,12 +21,14 @@ get '/locations' do
   cities = []
 
   if response_json["geonames"].nil?
+    p "response json for names is nil"
     success = false
   else
     cities = response_json["geonames"].collect do |x|
       { name: x["name"], lat: x["lat"], lng: x["lng"] }
     end
   end
+
   { success: success, cities: cities }.to_json
 end
 
